ebaeade67d0eefa24ba3e0232a17a5294fad2f91
Luan Pham <luanpm88@gmail.com>
2017-10-23 02:51:36 -0700

    edit contact with related info

--- app/decorators/controllers/orders/orders_controller_decorator.rb
+++ app/decorators/controllers/orders/orders_controller_decorator.rb
@@ -2,9 +2,19 @@ Erp::Orders::Backend::OrdersController.class_eval do
   # get related contact form
   def related_contact_form
     if !params.to_unsafe_hash[:datas].present?
-      render plain: 'sss'
+      render plain: ''
     else
-      @contact = Erp::Contacts::Contact.find(params.to_unsafe_hash[:datas][0])
+      @contact = Erp::Contacts::Contact.where(id: params.to_unsafe_hash[:datas][0]).first
+
+      if @contact.present?
+        if params.to_unsafe_hash[:order_id].present?
+          @order = Erp::Orders::Order.find(params.to_unsafe_hash[:order_id])
+        else
+          @order = Erp::Orders::Order.new
+        end
+      else
+        render plain: ''
+      end
     end
   end
 end
