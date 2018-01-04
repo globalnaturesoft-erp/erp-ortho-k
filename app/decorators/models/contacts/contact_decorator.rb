f73970f2bd9247659c50b2ad13459b7b8e040d89
Luan Pham <luanpm88@gmail.com>
2018-01-04 08:18:57 +0700

    fix contacts

--- app/decorators/models/contacts/contact_decorator.rb
+++ app/decorators/models/contacts/contact_decorator.rb
@@ -1,3 +1,16 @@
 Erp::Contacts::Contact.class_eval do
-    has_many :patient_orders, class_name: 'Erp::Orders::Order', foreign_key: :patient_id
+  has_many :patient_orders, class_name: 'Erp::Orders::Order', foreign_key: :patient_id
+
+  # import init stock from file
+  def self.import_init_contacts(file)
+    # config
+    timestamp = Time.now.to_i
+    xlsx = Roo::Spreadsheet.open(file)
+    user = Erp::User.first
+
+    # Read excel file. sheet tabs loop
+    xlsx.each_with_pagename do |name, sheet|
+
+    end
+  end
 end
