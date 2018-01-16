83154909a32c2ae74bac60b226479b7c039afe23
Hung Nguyen <hungnt@hoangkhang.com.vn>
2018-01-16 21:42:39 +0700

    update payment record

---
+++ app/decorators/models/payments/payment_record_decorator.rb
@@ -0,0 +1,14 @@
+Erp::Payments::PaymentRecord.class_eval do
+  # get report note
+  def get_report_note
+    str = []
+    str << I18n.t(".erp.payments.backend.#{pay_receive}_#{payment_type_code}")
+    if order.present?
+      if (!order.patient.nil? or !order.patient_state.nil?)
+        str << 'BN' + (' ' + order.patient_state_name if !order.patient.nil?) + (': ' + order.patient_name if !order.patient.nil?)
+      end
+    end
+    str << description.to_s if description.present?
+    return str.join(" - ")
+  end
+end
