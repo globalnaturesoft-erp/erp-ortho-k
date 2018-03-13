3dabb3060b7c7ec1a522dcf1fa1dc0f9416be377
Hung Nguyen <hungnt@hoangkhang.com.vn>
2018-03-13 10:10:09 +0700

    fix bug

--- app/decorators/models/orders/order_detail_decorator.rb
+++ app/decorators/models/orders/order_detail_decorator.rb
@@ -2,9 +2,10 @@ Erp::Orders::OrderDetail.class_eval do
   def display_eye_position
     if self.eye_position.present?
       if [Erp::Orders::Order::POSITION_LEFT,
-          Erp::Orders::Order::POSITION_RIGHT,
-          Erp::Orders::Order::POSITION_BOTH].include?(self.eye_position)
+          Erp::Orders::Order::POSITION_RIGHT].include?(self.eye_position)
         return I18n.t(".#{eye_position}_eye")
+      elsif [Erp::Orders::Order::POSITION_BOTH].include?(self.eye_position)
+        return I18n.t(".#{eye_position}_eyes")
       else
         return ''
       end
