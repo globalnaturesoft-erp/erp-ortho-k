4edcee8994400d5f6e00ce5935407b0a402f580c
Luan Pham <luanpm88@gmail.com>
2018-03-28 08:45:28 +0700

    fix bug

--- app/decorators/models/qdeliveries/delivery_detail_decorator.rb
+++ app/decorators/models/qdeliveries/delivery_detail_decorator.rb
@@ -2,11 +2,13 @@ Erp::Qdeliveries::DeliveryDetail.class_eval do
   # get report name
   def get_report_name
     str = []
-    str << get_order.doctor_name if get_order.present? and get_order.doctor_name.present?
-    str << ('BN' + ((' ' + get_order.patient_state_name) if get_order.patient_state_name.present?) + ': ' + get_order.patient_name) if get_order.patient_name.present?
-    str << get_order_code if !order_detail.nil?
-    str << get_order.order_date.strftime("%d/%m/%Y") if !order_detail.nil?
-    str << state_name if state_name.present?
+    if get_order.present?
+      str << get_order.doctor_name if get_order.doctor_name.present?
+      str << ('BN' + ((' ' + get_order.patient_state_name) if get_order.patient_state_name.present?) + ': ' + get_order.patient_name) if get_order.patient_name.present?
+      str << get_order_code if !order_detail.nil?
+      str << get_order.order_date.strftime("%d/%m/%Y") if !order_detail.nil?
+      str << state_name if state_name.present?
+    end
     return str.join(" - ")
   end
 end
