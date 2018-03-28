a322c28c8737c00b4f293d8276ab0b737aa5e6bd
Luan Pham <luanpm88@gmail.com>
2018-03-28 08:30:29 +0700

    fix bug

--- app/decorators/models/qdeliveries/delivery_detail_decorator.rb
+++ app/decorators/models/qdeliveries/delivery_detail_decorator.rb
@@ -2,7 +2,7 @@ Erp::Qdeliveries::DeliveryDetail.class_eval do
   # get report name
   def get_report_name
     str = []
-    str << get_order.doctor_name if get_order.doctor_name.present?
+    str << get_order.doctor_name if get_order.present? and get_order.doctor_name.present?
     str << ('BN' + ((' ' + get_order.patient_state_name) if get_order.patient_state_name.present?) + ': ' + get_order.patient_name) if get_order.patient_name.present?
     str << get_order_code if !order_detail.nil?
     str << get_order.order_date.strftime("%d/%m/%Y") if !order_detail.nil?
