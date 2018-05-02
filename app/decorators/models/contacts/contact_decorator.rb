f1cee8f44e52528d140174e86feafd5b54a3e7f8
Hung Nguyen <hungnt@hoangkhang.com.vn>
2018-05-02 16:43:59 +0700

    update filter list

--- app/decorators/models/contacts/contact_decorator.rb
+++ app/decorators/models/contacts/contact_decorator.rb
@@ -442,18 +442,18 @@ Erp::Contacts::Contact.class_eval do
     # Loc danh sach cac khach hang co phat sinh giao dich (thanh toan, chiet khau)    
     order_query = Erp::Orders::Order.all_confirmed
       .sales_orders
-      .payment_for_contact_orders(from_date: @from, to_date: @to)
+      .payment_for_contact_orders#(from_date: @from, to_date: @to)
       .select('customer_id')
       .where("cache_customer_commission_amount != ?", 0.0)
     
     # @todo truong hop don hang co chiet khau nhung da bi tra lai (tinh sao?)
     
-    payment_query = Erp::Payments::PaymentRecord.all_done
-      .select('customer_id')
-      .where(payment_type_id: Erp::Payments::PaymentType.find_by_code(Erp::Payments::PaymentType::CODE_CUSTOMER_COMMISSION).id)
-      .where("payment_date >= ? AND payment_date <= ?", @from, @to)
+    #payment_query = Erp::Payments::PaymentRecord.all_done
+    #  .select('customer_id')
+    #  .where(payment_type_id: Erp::Payments::PaymentType.find_by_code(Erp::Payments::PaymentType::CODE_CUSTOMER_COMMISSION).id)
+    #  .where("payment_date >= ? AND payment_date <= ?", @from, @to)
 
-    self.where("erp_contacts_contacts.id IN (?) OR erp_contacts_contacts.id IN (?)", order_query, payment_query)
+    self.where("erp_contacts_contacts.id IN (?)", order_query)
   end
   
   # Get patient by state and date
