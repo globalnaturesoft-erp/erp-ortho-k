e78025688e25302afdce8bb058c48deee88e2973
Luan Pham <luanpm88@gmail.com>
2018-05-11 16:26:23 +0700

    update report

--- app/controllers/erp/ortho_k/backend/sales_controller.rb
+++ app/controllers/erp/ortho_k/backend/sales_controller.rb
@@ -27,7 +27,8 @@ module Erp
             
             # order details rows
             @od_rows = @order_details.group_by { |d| d.order.patient_state }
-            @dd_rows = @delivery_details.group_by { |d| (d.delivery.get_related_order.present? ? d.delivery.get_related_order.patient_state : nil) }
+            @dd_rows = @delivery_details.group_by { |d| (d.get_patient_state.present? ? d.get_patient_state.id : 10000) }
+            @dd_rows = Hash[@dd_rows.sort_by{|k,v| k}]
           end
           
           File.open("tmp/report_sell_and_return_xlsx.yml", "w+") do |f|
