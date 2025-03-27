25a923eff5772b53636a8c18bbc310b1ae8b5e9a
Hung Nguyen Ton <hungnt@hoangkhang.com.vn>
2025-03-27 14:49:12 +0700

    update new patient report - v2

--- app/decorators/models/contacts/contact_decorator.rb
+++ app/decorators/models/contacts/contact_decorator.rb
@@ -165,7 +165,7 @@ Erp::Contacts::Contact.class_eval do
       .select('customer_id')
       .where(payment_type_id: Erp::Payments::PaymentType.find_by_code(Erp::Payments::PaymentType::CODE_CUSTOMER).id)
       .where("payment_date >= ? AND payment_date <= ?", @from, @to)
-    
+
     self.where("erp_contacts_contacts.id IN (?) OR erp_contacts_contacts.id IN (?) OR erp_contacts_contacts.id IN (?)",
                order_query, product_return_query, payment_query)
 
@@ -234,7 +234,7 @@ Erp::Contacts::Contact.class_eval do
       i_type_2 = 4
       i_type = 3
       i_parent = 11
-      
+
       i_district = 7
       i_city = 8
       i_salesperson = 12
@@ -283,12 +283,12 @@ Erp::Contacts::Contact.class_eval do
 
           group = Erp::Contacts::ContactGroup.where("LOWER(name) = ? ", row[i_group].value.downcase.strip).first
           contact.contact_group = group
-          
+
           # create group if not exist
           if group.nil?
             group = Erp::Contacts::ContactGroup.create(id: 8, name: row[i_group].value.strip)
           end
-          
+
           # create user if group is Nhân viên
           if group.name == 'Nhân viên'
             user_email = contact.name.to_ascii.downcase.split(' ').last.strip + "."
@@ -296,9 +296,9 @@ Erp::Contacts::Contact.class_eval do
               user_email += word[0]
             end
             user_email += '@fargo.vn'
-            
+
             user = Erp::User.where(email: user_email).first
-            
+
             if user.nil?
               user = Erp::User.create(
                 email: user_email,
@@ -309,7 +309,7 @@ Erp::Contacts::Contact.class_eval do
                 active: true
               )
             end
-            
+
             contact.user_id = user.id
           end
 
@@ -335,42 +335,42 @@ Erp::Contacts::Contact.class_eval do
 
           # puts contact.to_json
           exist = Erp::Contacts::Contact.where(name: contact.name).first
-          
+
           # district
           if row[i_district].present?
             district_name = row[i_district].value
-            
+
             district = Erp::Areas::District.where("name LIKE ? or LOWER(name) LIKE ?", district_name.strip, district_name.downcase.strip).first
-            
+
             contact.district = district
           end
-          
+
           # district
           if row[i_city].present?
             state_name = row[i_city].value
-            
+
             state = Erp::Areas::State.where("name LIKE ? or LOWER(name) LIKE ?", state_name.strip, state_name.downcase.strip).first
-            
+
             #puts "#{state.present?} - #{state_name}"
-            
+
             contact.state = state
           end
-          
+
           # country
           contact.country = Erp::Areas::Country.where(name: "Việt Nam").first
-          
+
           # salesperson
           if row[i_salesperson].present?
             sp_name = row[i_salesperson].value
-            
+
             salesperson = Erp::User.where(name: sp_name.strip).first
-            
+
             contact.salesperson = salesperson
           end
-          
-          # salesperson    
+
+          # salesperson
           contact.commission_percent = row[i_commission_percent].value
-          
+
           # contact.save
           printf "%-10s %-10s %-10s %-40s %-10s %-10s %-10s %-10s %-15s %-25s %-20s %-10s\n",
             row[i_num],
@@ -379,15 +379,15 @@ Erp::Contacts::Contact.class_eval do
             contact.name[0..30],
             (contact.is_supplier ? "Supplier" : ((contact.is_customer ? "Customer" : '####'))),
             contact.contact_type,
-            contact.contact_group_name,            
+            contact.contact_group_name,
             (contact.user.present? ? contact.user.name : ''),
             (contact.state.present? ? contact.state.name : ''),
             (contact.district.present? ? contact.district.name : ''),
             (contact.salesperson.present? ? contact.salesperson.name : ''),
             contact.commission_percent
-          
+
           exist = Erp::Contacts::Contact.where(name: contact.name).first
-          
+
           if exist.nil?
             contact.save
             puts "#{contact.valid?} ########### SAVED"
@@ -402,7 +402,7 @@ Erp::Contacts::Contact.class_eval do
       end
     end
   end
-  
+
   # get contacts list for payment chasing // Don hang ban le/PK
   def self.get_sales_orders_tracking_payment_chasing_contacts(options={})
     @from = options[:from_date]
@@ -423,7 +423,7 @@ Erp::Contacts::Contact.class_eval do
       .select('customer_id')
       .where(payment_type_id: Erp::Payments::PaymentType.find_by_code(Erp::Payments::PaymentType::CODE_SALES_ORDER).id)
       .where("payment_date >= ? AND payment_date <= ?", @from, @to)
-    
+
     #ids = [-1]
     #self.all.each do |c|
     #  if c.orders_tracking_sales_debt_amount > 0.0
@@ -433,21 +433,21 @@ Erp::Contacts::Contact.class_eval do
 
     self.where("erp_contacts_contacts.id IN (?) OR erp_contacts_contacts.id IN (?) OR erp_contacts_contacts.id IN (?)", order_query, product_return_query, payment_query)
   end
-  
+
   # get contacts list for payment chasing // Customer commission
   def self.get_customer_commission_payment_chasing_contacts(options={})
     @from = options[:from_date]
     @to = options[:to_date]
 
-    # Loc danh sach cac khach hang co phat sinh giao dich (thanh toan, chiet khau)    
+    # Loc danh sach cac khach hang co phat sinh giao dich (thanh toan, chiet khau)
     order_query = Erp::Orders::Order.all_confirmed
       .sales_orders
       .payment_for_contact_orders#(from_date: @from, to_date: @to)
       .select('customer_id')
       .where("cache_customer_commission_amount != ?", 0.0)
-    
+
     # @todo truong hop don hang co chiet khau nhung da bi tra lai (tinh sao?)
-    
+
     #payment_query = Erp::Payments::PaymentRecord.all_done
     #  .select('customer_id')
     #  .where(payment_type_id: Erp::Payments::PaymentType.find_by_code(Erp::Payments::PaymentType::CODE_CUSTOMER_COMMISSION).id)
@@ -455,18 +455,18 @@ Erp::Contacts::Contact.class_eval do
 
     self.where("erp_contacts_contacts.id IN (?)", order_query)
   end
-  
+
   # Get patient by state and date
   def self.get_patients_by_state(options={})
     patient_ids = Erp::Orders::Order.all_confirmed
     patient_ids = patient_ids.where(payment_for: options[:payment_for]) if options[:payment_for].present?
-    patient_ids = patient_ids.where.not(patient_id: nil)      
+    patient_ids = patient_ids.where.not(patient_id: nil)
     patient_ids = patient_ids.where(patient_state_id: options[:patient_state_id]) if options[:patient_state_id].present?
-    
+
     if options[:customer_id].present?
       patient_ids = patient_ids.where(customer_id: options[:customer_id])
     end
-    
+
     if options[:from].present?
       patient_ids = patient_ids.where('order_date >= ?', options[:from].beginning_of_day)
     end
@@ -474,56 +474,56 @@ Erp::Contacts::Contact.class_eval do
     if options[:to].present?
       patient_ids = patient_ids.where('order_date <= ?', options[:to].end_of_day)
     end
-    
+
     patient_ids
   end
-  
+
   # Get contact has returned products
   def self.get_has_sales_returned_qdeliveries(options={})
     query = Erp::Qdeliveries::DeliveryDetail.get_returned_confirmed_delivery_details(options)
     query = query.select('erp_qdeliveries_deliveries.customer_id')
-    
+
     self.where(id: query)
   end
-  
+
   # START - Update cache sales/purchase debt amount
   after_save :update_cache_sales_debt_amount
   def update_cache_sales_debt_amount
     self.update_column(:cache_sales_debt_amount, self.sales_debt_amount)
   end
-  
+
   after_save :update_cache_purchase_debt_amount
   def update_cache_purchase_debt_amount
     self.update_column(:cache_purchase_debt_amount, self.purchase_debt_amount)
   end
   # END - Update cache sales/purchase debt amount
-  
+
   def self.get_sales_debt_amount_residual_contacts(options={})
     self.all_active.where("erp_contacts_contacts.cache_sales_debt_amount != ?", 0.0)
   end
-  
+
   def self.get_purchase_debt_amount_residual_contacts(options={})
     self.all_active.where("erp_contacts_contacts.cache_purchase_debt_amount != ?", 0.0)
   end
-  
+
   # Get liabilities contacts //in_period_active and is_debt_active
   def self.get_sales_liabilities_contacts(options={})
     self.get_sales_payment_chasing_contacts(options).or(self.get_sales_debt_amount_residual_contacts)
   end
-  
+
   def self.get_purchase_liabilities_contacts(options={})
     self.get_purchase_payment_chasing_contacts(options).or(self.get_purchase_debt_amount_residual_contacts)
   end
-  
+
   # cache_sales_debt_amount
   def self.cache_sales_debt_amount
     self.sum("erp_contacts_contacts.cache_sales_debt_amount")
   end
-  
+
   CONTACT_GROUPS_ALL = 'contact_all'
   CONTACT_GROUPS_FARGO_HN = 'contact_is_fargo_hn'
   CONTACT_GROUPS_NOT_FARGO_HN = 'contact_is_not_fargo_hn'
-  
+
   def self.get_contact_groups()
     [
       {
@@ -540,7 +540,7 @@ Erp::Contacts::Contact.class_eval do
       }
     ]
   end
-  
+
   # data for dataselect ajax
   def self.dataselect(keyword='', params='')
 
@@ -565,7 +565,7 @@ Erp::Contacts::Contact.class_eval do
     if params[:parent_id].present?
       query = query.where(parent_id: params[:parent_id])
     end
-    
+
     if keyword.present?
       keyword = keyword.strip.downcase
       keyword.split(' ').each do |q|
@@ -577,20 +577,20 @@ Erp::Contacts::Contact.class_eval do
     if params[:contact_id].present?
       query = query.where.not(id: params[:contact_id])
     end
-    
+
     if params[:contact_group_id] == Erp::Contacts::ContactGroup::GROUP_DOCTOR.to_s
       # style #1
       if params[:customer].present?
         query = query.where(parent_id: params[:customer])
       end
-      
+
       # style #2
       if params[:customer_id].present?
         query = query.where(parent_id: params[:customer_id])
       end
     end
-    
-    if params[:contact_group_id] == Erp::Contacts::ContactGroup::GROUP_PATIENT.to_s      
+
+    if params[:contact_group_id] == Erp::Contacts::ContactGroup::GROUP_PATIENT.to_s
       # style #1
       if params[:customer].present?
         if params[:doctor].present?
@@ -605,7 +605,7 @@ Erp::Contacts::Contact.class_eval do
           query = query.where(parent_id: params[:doctor])
         end
       end
-      
+
       # style #2
       if params[:customer_id].present?
         if params[:doctor_id].present?
@@ -620,7 +620,7 @@ Erp::Contacts::Contact.class_eval do
           query = query.where(parent_id: params[:doctor_id])
         end
       end
-      
+
       # Danh sách bệnh nhân trên phiếu hoàn kho bán/xuất bán/nhập kho mua hàng/xuất trả ncc (không chứng từ) <lấy theo tên khách hàng tương ứng>
       if params[:delivery].present?
         if params[:delivery][:customer_id].present?
@@ -632,5 +632,13 @@ Erp::Contacts::Contact.class_eval do
     end
 
     query = query.limit(25).map{|contact| {value: contact.id, text: contact.contact_name} }
+  end
+
+  # Update cache sales debt amount
+  def self.update_cache_sales_debt_amount
+    self.all.each do |contact|
+      contact.update_cache_sales_debt_amount
+    end
+    puts (Time.now.to_s + ": update all " + self.all.count.to_s)
   end
 end
